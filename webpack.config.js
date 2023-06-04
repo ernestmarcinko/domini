6a37c5478ea0474cf808b00620308764d44b3400
anag0 <ernest.marcinko@gmail.com>
2023-06-04 17:21:12 +0200

    webpack config  for individual modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = [{
-        entry: './src/DoMini.js',
+        entry: './src/domini.js',
         output: {
             library: {
                 name: 'DoMini',
@@ -8,23 +8,102 @@ module.exports = [{
                 export: 'default',
                 umdNamedDefine: true
             },
-            globalObject: 'this',
+            globalObject: 'window',
             path: path.resolve(__dirname, 'dist'),
             filename: 'domini.js',
         }
     },
     {
-        entry: './src/DoMiniCore.js',
+        entry: './src/domini-core.js',
         output: {
             library: {
                 name: 'DoMini',
-                type: 'umd',
+                type: 'global',
                 export: 'default',
-                umdNamedDefine: true
             },
-            globalObject: 'this',
+            globalObject: 'window',
             path: path.resolve(__dirname, 'dist'),
             filename: 'domini-core.js',
+        }
+    },
+    {
+        entry: './src/domini-animate.js',
+        externals: {
+            '../base': 'DoMini'
+        },
+        output: {
+            library: {
+                name: 'DoMini',
+                type: 'global',
+                export: 'default',
+            },
+            globalObject: 'window',
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'domini-animate.js',
+        }
+    },
+    {
+        entry: './src/domini-highlight.js',
+        externals: {
+            '../base': 'DoMini'
+        },
+        output: {
+            library: {
+                name: 'DoMini',
+                type: 'global',
+                export: 'default',
+            },
+            globalObject: 'window',
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'domini-highlight.js',
+        }
+    },
+    {
+        entry: './src/domini-serialize.js',
+        externals: {
+            '../base': 'DoMini'
+        },
+        output: {
+            library: {
+                name: 'DoMini',
+                type: 'global',
+                export: 'default',
+            },
+            globalObject: 'window',
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'domini-serialize.js',
+        }
+    },
+    {
+        entry: './src/domini-viewport.js',
+        externals: {
+            '../base': 'DoMini'
+        },
+        output: {
+            library: {
+                name: 'DoMini',
+                type: 'global',
+                export: 'default',
+            },
+            globalObject: 'window',
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'domini-viewport.js',
+        }
+    },
+    {
+        entry: './src/domini-xhttp.js',
+        externals: {
+            '../base': 'DoMini'
+        },
+        output: {
+            library: {
+                name: 'DoMini',
+                type: 'global',
+                export: 'default',
+            },
+            globalObject: 'window',
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'domini-xhttp.js',
         }
     }
 ];
