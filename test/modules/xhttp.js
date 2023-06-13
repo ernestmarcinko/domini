3e9ea67a5aebca078ac8b5eafb3c43f9eeab61c0
anag0 <ernest.marcinko@gmail.com>
2023-06-13 15:14:40 +0200

    xhttp fixes in build

--- test/modules/xhttp.js
+++ test/modules/xhttp.js
@@ -5,11 +5,12 @@ QUnit.module("module: xhttp.js tests", function(hooks) {
         f.innerHTML = fixture;
     });
 
-    QUnit.test('xhttp()', function(assert) {
+    QUnit.test('xhttp() cors', function(assert) {
         const done = assert.async();
         let data = {
             'options': $('form[name=testform]').serialize()
         };
+        // Cors
         $.fn.ajax({
             "url": "/request",
             "method": "POST",
@@ -17,6 +18,32 @@ QUnit.module("module: xhttp.js tests", function(hooks) {
             "success": function(args){
                 assert.deepEqual(JSON.parse(args), data);
                 done();
+            }
+        });
+    });
+
+    QUnit.test('xhttp() no-cors', function(assert) {
+        const done = assert.async();
+        // No-Cors
+        DoMini.fn.ajax({
+            url: 'https://clients1.google.com/complete/search',
+            cors: 'no-cors',
+            data: {
+                q: 'us',
+                hl: 'en',
+                nolabels: 't',
+                client: 'hp',
+                ds: ''
+            },
+            success: function (data) {
+                if (data[1].length > 0) {
+                    let response = data[1][0][0].replace(/(<([^>]+)>)/ig, "");
+                    response = DoMini('<textarea />').html(response).text();
+                    console.log(response);
+                    assert.notEqual(response, '');
+                    assert.notEqual(response, 'us');
+                    done();
+                }
             }
         });
     });
